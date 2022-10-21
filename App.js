2b136ab1b5850d9dc14720eb39d3f5302b9ac819
XaadKhalid <khalidxaad@gmail.com>
2022-10-21 22:06:46 +0500

    first_change

--- App.js
+++ App.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import {Text, View, Button} from 'react-native';
-import {Login} from './Components/Login';
+import Login from './Components/Login';
 
 const App = () => {
-  function lg() {
-    <Login />;
-  }
   return (
     <View>
       <Text>Please login to see the details of project.</Text>
-      <Button title="Login" onPress={lg} />
+      <Button
+        title="Login"
+        onPress={() => {
+          alert('Press Ok to Close the Alert box!');
+        }}
+      />
+      <Login />
     </View>
   );
 };
